f2580351f5cb2ba2eb0d68208ec7b27cdddb0bd7
AJokilahti <aleksi.jokilahti@myy.haaga-helia.fi>
2022-03-29 14:30:10 +0300

    Init backend

---
+++ KyselyApp/src/main/java/com/abu/dhabi/KyselyApp/KyselyAppApplication.java
@@ -0,0 +1,13 @@
+package com.abu.dhabi.KyselyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselyAppApplication.class, args);
+	}
+
+}
---
+++ KyselyApp/src/test/java/com/abu/dhabi/KyselyApp/KyselyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abu.dhabi.KyselyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
