5f20c6c2bc021f49e5115c750c35f9129df1ca31
karei <eetu.kivelae@gmail.com>
2022-03-31 09:49:51 +0300

    Added tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button class="bg-gray-800" type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    "./index.html",
+    "../KyselyApp/src/main/resources/templates/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
