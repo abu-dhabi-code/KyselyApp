68a7cf0f7cebfeb11e61ff2beedec8507d617bf5
karei <eetu.kivelae@gmail.com>
2022-03-31 09:23:56 +0300

    Disabled hashing for .css filenames Makes it easier to work with for Spring Boot templates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../KyselyApp/src/main/resources/static/'
+    outDir: '../KyselyApp/src/main/resources/static/',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]'
+      }
+    }
   }
 })
