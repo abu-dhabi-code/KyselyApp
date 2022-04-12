923b5cf17b82d330edadf4424e7023e50e9a221e
AJokilahti <aleksi.jokilahti@myy.haaga-helia.fi>
2022-04-12 12:23:07 +0300

    Added Answer entity class

---
+++ KyselyApp/src/main/java/com/abu/dhabi/KyselyApp/domain/Answer.java
@@ -0,0 +1,80 @@
+package com.abu.dhabi.KyselyApp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.lang.NonNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+public class Answer {
+
+	@Id
+	@NonNull
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String answer;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("answers")
+	@JoinColumn(name="questionid")
+	@NonNull
+	private Question question;
+	
+	// Text is default answer type for now
+	private QuestionType type = QuestionType.Text;
+	
+	public Answer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Answer(String answer) {
+		super();
+		this.answer = answer;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public QuestionType getType() {
+		return type;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public void setType(QuestionType type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Answer [id=" + id + ", answer=" + answer + ", question=" + question + ", type=" + type + "]";
+	}
+	
+	
+}
