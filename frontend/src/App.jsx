a6519b24e161972c6245b01004c1102fcb3863a8
Julia Söderlund <julia.soderlund@yandex.ru>
2022-04-19 12:24:39 +0300

    added a header fragment for thymeleaf pages and also added a link that leads to the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import Survey from "./pages/Survey"
@@ -9,7 +9,11 @@ function App() {
     <div className="flex flex-col items-center h-full">
       <div className="bg-teal-blue w-full text-center flex flex-row items-center justify-center">
         <span className="invisible ml-4 mr-auto w-28"></span>
-        <h1 className="text-3xl mt-2 mb-2">KyselyApp</h1>
+        <Link
+          className="text-3xl mt-2 mb-2"
+          to={"/"}>
+            KyselyApp
+        </Link>
         <a href="/addsurvey" className="btn-primary ml-auto mr-4">Create survey</a>
       </div>
       <Routes>
