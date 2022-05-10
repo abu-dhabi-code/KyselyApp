4a42ddbfb0716c6ca8ed76fbb0c1a826e1946238
karei <eetu.kivelae@gmail.com>
2022-05-10 13:39:11 +0300

    Implement answering to multiselect questions

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -2,109 +2,146 @@
 
 function Question({ question, key, answerState }) {
 
-    const [answers, setAnswers] = answerState;
+  const [answers, setAnswers] = answerState;
 
-    function getAnswerByQuestionId(id) {
-        const results = answers.filter(a => a.id === id);
+  function getAnswerByQuestionId(id) {
+    const results = answers.filter(a => a.id === id);
 
-        return results.length > 0 ? results[0].text : '';
-    }
+    return results.length > 0 ? results[0].value : '';
+  }
 
-    function updateAnswerByQuestionId(newText, id) {
-        setAnswers(answers.map(a => {
-            // console.log(a)
-            if (a.id === id)
-                a.text = newText;
-            return a;
-        }));
+  function updateAnswerByQuestionId(newValue, id) {
+    console.log(answers);
+    const modAnswers = [...answers];
 
-    }
-
-    function getAnswerComponent(question) {
-        console.log(question.type)
-        switch(question.type) {
-            case "LongText":
-                return(
-                    <textarea
-                        placeholder="Answer"
-                        className="bg-white text-black px-2 mx-5 rounded textbox-width"
-                        value={getAnswerByQuestionId(question.id)}
-                        onChange={e => updateAnswerByQuestionId(e.currentTarget.value, question.id)}
-                    />  
-                )
-                case "Radio":
-                    return (
-                        <div className="flex flex-col gap-4 w-full">
-                        {question.options.map(o => (
-                        <label>
-                            {o.option}
-                            <input 
-                                name={question.id + question.question}
-                                key={o.id + o.option}
-                                type="radio"
-                                className="bg-white text-black px-2 mx-5 rounded"
-                                
-                                onChange={e => updateAnswerByQuestionId(o.option, question.id)}
-                            /> 
-                        </label>
-                    ))}
-                    </div>
-                    )
-                    case "Multiselect":
-                        return (
-                            <div className="flex flex-col gap-4 w-full">
-                            {question.options.map(o => (
-                            <label>
-                                {o.option}
-                                <input 
-                                    name={question.id + question.question}
-                                    key={o.id + o.option}
-                                    type="checkbox"
-                                    className="bg-white text-black px-2 mx-5 rounded"
-                                    
-                                    onChange={e => console.log(e)}
-                                /> 
-                            </label>
-                        ))}
-                        </div>
-                        )
-            default: 
-                return(
-                    <input
-                        type="text"
-                        placeholder="Answer"
-                        className="bg-white text-black px-2 mx-5 rounded textbox-width"
-                        value={getAnswerByQuestionId(question.id)}
-                        onChange={e => updateAnswerByQuestionId(e.currentTarget.value, question.id)}
-                    />
-                    )
+    switch (question.type) {
+      case "Multiselect":
+        const a = modAnswers.find(a => a.id === id);
+        if (a.value) {
+          const values = a.value;
+          const idx = values.indexOf(newValue)
+          if (idx > -1)
+            values.splice(idx, 1);
+          else
+            values.push(newValue);
+            
+        } else {
+          a.value = [newValue];
         }
+        break;
+
+      default:
+        modAnswers.find(a => a.id === id).value = newValue;
+        break;
     }
 
-    return (
-        <div
-            key={key}
-            className="bg-sweet-brown 
+    /* setAnswers(answers.map(a => {
+      // console.log(a)
+      if (a.id === id)
+        a.value = newValue;
+      return a;
+    }));
+ */
+    setAnswers(modAnswers);
+  }
+
+  function isCheckboxValueInAnswers(option, id) {
+    const a = answers.find(a => a.id === id);
+    if(!a)
+      return false;
+
+    if (a.value.length) {
+      return a.value.includes(option);
+    }
+    return false;
+  }
+
+  function getAnswerComponent(question) {
+    console.log(question.type)
+    switch (question.type) {
+      case "LongText":
+        return (
+          <textarea
+            placeholder="Answer"
+            className="bg-white text-black px-2 mx-5 rounded textbox-width"
+            value={getAnswerByQuestionId(question.id)}
+            onChange={e => updateAnswerByQuestionId(e.currentTarget.value, question.id)}
+          />
+        )
+      case "Radio":
+        return (
+          <div className="flex flex-col gap-4 w-full">
+            {question.options.map(o => (
+              <label>
+                {o.option}
+                <input
+                  name={question.id + question.question}
+                  key={o.id + o.option}
+                  type="radio"
+                  className="bg-white text-black px-2 mx-5 rounded"
+
+                  onChange={e => updateAnswerByQuestionId(o.option, question.id)}
+                />
+              </label>
+            ))}
+          </div>
+        )
+      case "Multiselect":
+        return (
+          <div className="flex flex-col gap-4 w-full">
+            {question.options.map(o => (
+              <label>
+                {o.option}
+                <input
+                  name={question.id + question.question}
+                  key={o.id + o.option}
+                  type="checkbox"
+                  className="bg-white text-black px-2 mx-5 rounded"
+
+                  checked={isCheckboxValueInAnswers(o.option, question.id)}
+                  onChange={e => updateAnswerByQuestionId(o.option, question.id)}
+                />
+              </label>
+            ))}
+          </div>
+        )
+      default:
+        return (
+          <input
+            type="text"
+            placeholder="Answer"
+            className="bg-white text-black px-2 mx-5 rounded textbox-width"
+            value={getAnswerByQuestionId(question.id)}
+            onChange={e => updateAnswerByQuestionId(e.currentTarget.value, question.id)}
+          />
+        )
+    }
+  }
+
+  return (
+    <div
+      key={key}
+      className="bg-sweet-brown 
                 m-3
                 px-1 py-1 rounded shadow-sm
                 flex flex-col items-center"
-        >
-            <div
-                className="w-full px-5 py-3 
+    >
+      <div
+        className="w-full px-5 py-3 
                 mt-0 mx-0 bg-dk-byzantium
                 rounded-t"
-            >
-                <h1 className="">
-                    {question.name}
-                </h1>
-            </div>
-            <div className="bg-dk-byzantium text-center brightness-95 rounded-b w-full pb-2">
-                {
-                    getAnswerComponent(question)
-                }
-            </div>
-        </div>
-    )
+      >
+        <h1 className="">
+          {question.name}
+        </h1>
+      </div>
+      <div className="bg-dk-byzantium text-center brightness-95 rounded-b w-full pb-2">
+        {
+          getAnswerComponent(question)
+        }
+      </div>
+    </div>
+  )
 }
 
 export default Question;
--- frontend/src/pages/Survey.jsx
+++ frontend/src/pages/Survey.jsx
@@ -5,80 +5,96 @@ import Success from "../components/Success";
 import { getSurvey, sendAnswers } from "../utils/api";
 
 function Survey() {
-    const id = parseInt(useParams().id);
-    const [survey, setSurvey] = useState();
-    const [answers, setAnswers] = useState([]);
-    const [showSuccess, setShowSuccess] = useState(false);
+  const id = parseInt(useParams().id);
+  const [survey, setSurvey] = useState();
+  const [answers, setAnswers] = useState([]);
+  const [showSuccess, setShowSuccess] = useState(false);
 
-    console.log(id);
+  console.log(id);
 
-    useEffect(() => {
-        getSurvey(id)
-            .then(v => {
-                setSurvey(v);
-            });
-    }, []);
+  useEffect(() => {
+    getSurvey(id)
+      .then(v => {
+        setSurvey(v);
+      });
+  }, []);
 
-    useEffect(() => {
-        if (survey)
-            setAnswers(survey.questions.map(q => {
-                return {
-                    id: q.id,
-                    text: ''
-                }
-            }));
-    }, [survey]);
+  useEffect(() => {
+    if (survey)
+      setAnswers(survey.questions.map(q => {
+        return {
+          id: q.id,
+          type: q.type,
+          value: ''
+        }
+      }));
+  }, [survey]);
 
-    function submitAnswers() {
-        console.log(answers);
-        const answerObject = answers.map(e => {
-            return {
-                answer: e.text,
-                question: {
-                    id: e.id
-                }
-            }
+  function submitAnswers() {
+    console.log(answers);
+    const answerObjects = answers
+      .filter(e => e.type !== "Multiselect")
+      .map(e => {
+        return {
+          answer: e.value,
+          question: {
+            id: e.id
+          }
+        }
+      })
+
+    const multiAnswers = answers.filter(e => e.type === "Multiselect");
+    for (const answer of multiAnswers)
+      for (const option of answer.value)
+        answerObjects.push({
+          answer: option,
+          question: {
+            id: answer.id
+          }
         });
-        sendAnswers(answerObject)
-            .then(res => console.log(res))
+
+    sendAnswers(answerObjects)
+      .then(res => {
+        console.log(res)
         setShowSuccess(true);
-    }
+      })
+  }
 
-    return (
-        <div className="flex flex-col items-center h-full">
-            <h1 className="text-2xl my-2">{survey?.surveyName}</h1>
-            <p>{survey?.description}</p>
-            <div className="mt-2">
-                {!showSuccess &&
-                (survey
-                    ?
-                    <div className="flex flex-col items-center">
-                        {(survey.questions.map(question => (
-                            <span key={question.id} >
-                                <Question
-                                    question={question}
-                                    answerState={[answers, setAnswers]}
-                                />
-                            </span>
-                        )))}
-                    <button className="btn-primary mt-auto mb-4" onClick={submitAnswers}>Submit answers</button>
-                    </div>
-                    :
-                    (
-                        <h1>Loading</h1>
-                    )
-                    
-                )
-                }
-                {showSuccess &&
-                    <Success />
-
-                }
+  return (
+    <div className="flex flex-col items-center h-full">
+      <h1 className="text-2xl my-2">{survey?.surveyName}</h1>
+      <p>{survey?.description}</p>
+      <div className="mt-2">
+        {!showSuccess &&
+          (survey
+            ?
+            <div className="flex flex-col items-center">
+              {(survey.questions.map(question => (
+                <span key={question.id} >
+                  <Question
+                    question={question}
+                    answerState={[answers, setAnswers]}
+                  />
+                </span>
+              )))}
+              <button className="btn-primary mt-auto mb-4" onClick={submitAnswers}>Submit answers</button>
             </div>
-            
-            <Link className="btn-primary mt-auto mb-4" to="/">Go back home</Link>
-        </div>
-    )
+            :
+            (
+              <h1>Loading</h1>
+            )
+
+          )
+        }
+        {showSuccess &&
+          <Success />
+
+        }
+      </div>
+
+      <Link className="btn-primary mt-auto mb-4" to="/">Go back home</Link>
+    </div>
+  )
 }
 
 export default Survey;
