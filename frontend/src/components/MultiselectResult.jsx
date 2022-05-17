72d2be4e809f46dbcec18055476597e5093cbeb5
Julia Söderlund <julia.soderlund@yandex.ru>
2022-05-17 13:06:58 +0300

    made the bar chart to work better with dark mode

--- frontend/src/components/MultiselectResult.jsx
+++ frontend/src/components/MultiselectResult.jsx
@@ -1,35 +1,44 @@
 import { useState, useEffect } from "react";
 import { Chart } from "react-google-charts";
 
-function MultiselectResult({ answerObjects }){
+function MultiselectResult({ answerObjects }) {
 
-    const [data, setData] = useState();
+  const [data, setData] = useState();
 
-    const options = {
-        backgroundColor: "#482C3D",
-        
-        width: "100%",
-        height: 300,
-        legend: "none"
-    };
+  const options = {
+    backgroundColor: "#482C3D",
+    width: "100%",
+    height: "100%",
+    /* bar: { groupWidth: "95%" }, */
+    hAxis: {
+      textStyle: {color: 'white'},
+      gridlines: {color: '#787878'},
+      minorGridlines: {count:0},
+      /* titleTextStyle: {color: 'white'}, */
+    },
+    vAxis: {
+      textStyle: {color: 'white'},
+    },
+    legend: "none"
+  };
 
-    useEffect(() => {
-        let temp = [["answer", "Times answered"]]
-        answerObjects.forEach( answer => {
-            temp = [...temp, [answer.answer, answer.count]]
-        })
-        setData(temp);
-    }, []);
+  useEffect(() => {
+    let temp = [["answer", "Times answered"]]
+    answerObjects.forEach(answer => {
+      temp = [...temp, [answer.answer, answer.count]]
+    })
+    setData(temp);
+  }, []);
 
-    return(
-        <div className="flex flex-col gap-4 pt-2 pl-6 justify-center items-start w-full">
-            <Chart
-            chartType="BarChart"
-            data={data}
-            options={options}
-            />
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-4 pt-2 pl-6 justify-center items-start w-full">
+      <Chart
+        chartType="BarChart"
+        data={data}
+        options={options}
+      />
+    </div>
+  )
 }
 
 export default MultiselectResult;
