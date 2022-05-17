1bfad1c6b6a17305c0129e969c05b366a98c1019
AJokilahti <aleksi.jokilahti@myy.haaga-helia.fi>
2022-05-17 11:46:52 +0300

    Blocked saving empty answers

--- frontend/src/components/MultiselectResult.jsx
+++ frontend/src/components/MultiselectResult.jsx
@@ -1,35 +1,35 @@
 import { useState, useEffect } from "react";
 import { Chart } from "react-google-charts";
 
-function MultiselectResult({ answerObjects }){
+function MultiselectResult({ answerObjects }) {
 
-    const [data, setData] = useState();
+  const [data, setData] = useState();
 
-    const options = {
-        backgroundColor: "#482C3D",
-        
-        width: "100%",
-        height: 300,
-        legend: "none"
-    };
+  const options = {
+    backgroundColor: "#482C3D",
 
-    useEffect(() => {
-        let temp = [["answer", "Times answered"]]
-        answerObjects.forEach( answer => {
-            temp = [...temp, [answer.answer, answer.count]]
-        })
-        setData(temp);
-    }, []);
+    width: "100%",
+    height: 300,
+    legend: "none"
+  };
 
-    return(
-        <div className="flex flex-col gap-4 pt-2 pl-6 justify-center items-start w-full">
-            <Chart
-            chartType="BarChart"
-            data={data}
-            options={options}
-            />
-        </div>
-    )
+  useEffect(() => {
+    let temp = [["answer", "Times answered"]]
+    answerObjects.forEach(answer => {
+      temp = [...temp, [answer.answer, answer.count]]
+    })
+    setData(temp);
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-4 pt-2 pl-6 justify-center items-start w-full">
+      <Chart
+        chartType="BarChart"
+        data={data}
+        options={options}
+      />
+    </div>
+  )
 }
 
 export default MultiselectResult;
